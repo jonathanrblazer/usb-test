48a423d87d33f1650a9d6afaa087582588be4774
jonathanrblazer <jonathan.r.blazer@gmail.com>
2025-12-27 18:30:17 -0500

    increased image size; going to add disparity overlays to image

--- main.cpp
+++ main.cpp
@@ -668,7 +668,7 @@ int main() {
 
             cv::Mat disp = makeStereoDisplay(localCopy);
             cv::bitwise_not(disp, disp);
-            cv::resize(disp, disp, {}, 3.0, 3.0, cv::INTER_NEAREST);
+            cv::resize(disp, disp, {}, 6.0, 6.0, cv::INTER_NEAREST);
             cv::imshow("Stereo", disp);
         }
 
